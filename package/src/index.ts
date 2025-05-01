21a44e4fb7d29d633223ad4176fe9c0e4a139c15
Henry Paulino <me@henrypl.com>
2025-05-01 19:28:46 -0400

    fix

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,9 +1,9 @@
 // Export types
-export * from './types'
+export * from "./types";
 
 // Export hybrid objects
-export { HybridSpotifyAuth } from './lib/spotifyAuth'
-export { HybridSpotifyPlayer } from './lib/spotifyPlayer'
+// export { HybridSpotifyAuth } from './lib/spotifyAuth'
+// export { HybridSpotifyPlayer } from './lib/spotifyPlayer'
 
 // Export hook
-export { useSpotify } from './hooks/useSpotify'
+export { useSpotify } from "./hooks/useSpotify";
